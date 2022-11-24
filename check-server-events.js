fb6c1f76d495e6fb809aac75aaa565c2f6a58cc0
Ivan Grekov <ivan.grekov@windmill.digital>
2022-11-24 08:45:29 +0200

    checking servers events

---
+++ check-server-events.js
@@ -0,0 +1,27 @@
+import http from 'http';
+
+console.clear();
+
+const server = new http.Server();
+
+server.on('request', (_, res) => {
+    res.setHeader('Content-Type', 'text/html');
+    res.write('Hello, world!');
+    res.end();
+});
+
+const nativeEmit = server.emit;
+server.emit = (...args) => {
+    console.log(args[0]);
+
+    // nativeEmit.apply(server, args);
+    http.Server.prototype.emit.apply(server, args);
+};
+
+server.listen(3000, () => {
+    console.log('server is running, http://localhost:3000');
+});
+
+console.log(server);
+
+console.log('eventNames', server.eventNames());

a89a7705f72b583bab9b3d70fa2ebe0fd2bba0b5
Ivan Grekov <ivan.grekov@windmill.digital>
2022-11-24 18:14:10 +0200

    readable writable streams

--- stream-types.js
+++ stream-types.js
@@ -1,12 +1,20 @@
 import { Readable, Writable, Duplex, Transform } from 'stream';
+import fs from 'fs';
 
 // Buffer size - 64kb
 
-// Readable stream (req)
+// Readable stream
 new Readable();
+fs.createReadStream('index.html');
+process.stdin;
+req;
 
-// Writable stream (res)
+// Writable stream
 new Writable();
+fs.createWriteStream('new-file.html');
+process.stdout;
+process.stderr;
+res;
 
 // Duplex stream (can be written and read)
 new Duplex();
